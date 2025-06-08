5f09b4713999cab4590c1551127de251b11d01bb
jpellogo01 <jpellogo01@gmail.com>
2025-06-08 14:28:25 +0800

    Initial Commit

---
+++ index.js
@@ -0,0 +1,9 @@
+let navLinks = document.getElementById("navLinks");
+
+function hideMenu(){
+    navLinks.style.right ="-200px";
+}
+function showMenu(){
+    navLinks.style.right ="0";
+    navLinks.style.display ="block";
+}
